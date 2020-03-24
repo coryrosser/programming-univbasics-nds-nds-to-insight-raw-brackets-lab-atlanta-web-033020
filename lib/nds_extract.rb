7e20262cf640002ee19591fecae364ec3ec93f93
Cortlyn Rosser <coryrosser@gmail.com>
2020-03-24 12:42:51 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,18 +4,15 @@ require 'pp'
 
 
 def directors_totals(nds)
-   
-   nds = directors_database
-   
+   new_hash = {}
    row_i = 0 
    while row_i < nds.length do
-     new_hash = {}
-     total = 0
+     total[name] = 0
      movie = nds[row_i][:movies]
      name = nds[row_i][:name]
      col_i = 0 
      while col_i < movie.length do
-      total = total + movie[col_i][:worlwide_gross]
+      total[name] += movie[col_i][:worlwide_gross]
       col_i += 1
     end
     new_hash[:name] = total
