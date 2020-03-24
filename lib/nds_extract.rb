1313d95c0607264dc9df34284e2ef51af810aaa9
Cortlyn Rosser <coryrosser@gmail.com>
2020-03-24 12:49:34 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,3 +21,20 @@ def directors_totals(nds)
   new_hash
     
 end
+
+
+def directors_totals(nds)
+  director_index = 0
+  total = {}
+  while director_index < nds.length do 
+       movie_index = 0
+       director_name = nds[director_index][:name]
+       total[director_name] = 0        #building dictionary adding to total{}
+       while movie_index < nds[director_index][:movies].length do 
+         total[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
+          movie_index += 1
+         end     
+       director_index += 1 
+  end
+  total
+end
